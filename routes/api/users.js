bd74a2dc29253ce8776d063daffbe14c3aa68e83
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-18 12:55:12 +0200

    He hecho el modelo de usuario y alguna ruta de usuarios

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,36 @@
 const router = require( 'express' ).Router()
+const User = require( '../../models/user.model' )
+const BookClub = require( '../../models/book_club.model' )
 
-
-router.get( '/', ( req, res ) => {
-	res.json( { success: 'success' } )
+router.get( '/historial/:user_id', async ( req, res ) => {
+	try {
+		res.json( await User.getHistorial( req.params.user_id ) )
+	} catch ( err ) {
+		res.json( { error: err.message } )
+	}
 } )
-
+router.get( '/suscripciones/:user_id', async ( req, res ) => {
+	try {
+		res.json( await User.getSubscriptions( req.params.user_id ) )
+	} catch ( err ) {
+		res.json( { error: err.message } )
+	}
+} )
+router.get( '/:user_id/book_club', async ( req, res ) => {
+	let user = await User.getOne( req.params.user_id )
+	user.book_club = await BookClub.getAllByAdmin( req.params.user_id )
+	try {
+		res.json( user )
+	} catch ( err ) {
+		res.json( { error: err.message } )
+	}
+} )
+router.get( '/:user_id', async ( req, res ) => {
+	try {
+		res.json( await User.getOne( req.params.user_id ) )
+	} catch ( err ) {
+		res.json( { error: err.message } )
+	}
+} )
 
 module.exports = router
